871557c92a6adf61fe4c32414c05fb5ab9a0df25
Juan Felipe Sánchez Pérez <juan.sanchez-pe@mail.escuelaing.edu.co>
2024-02-13 09:16:36 -0500

    Endpoint de H2-Console funcionando

--- src/main/java/edu/escuelaing/ieti/h2project/repository/CountryRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.escuelaing.ieti.h2project.repository;
-
-import edu.escuelaing.ieti.h2project.model.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CountryRepository extends JpaRepository<Country, Long> {
-
-}
